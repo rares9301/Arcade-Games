a618426dc62950c64ac8a8370af625e0469803cb
ap0 <rares.sarmasag@stud.acs.upb.ro>
2024-08-13 15:41:30 +0300

    fixed the raw input method

--- src/sudoku.cxx
+++ src/sudoku.cxx
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <cstring>
+#include <cstdio>
+#include <unistd.h>
+#include <termios.h>
 using namespace std;
 
 char sudoku[9][9];
 
-void RawMode(bool start) {
-    
+// Used C++ refference for termios.h to come up with this raw reading mode
+void RawMode(bool enable) {
+  static struct termios oldt, newt;
+    if (enable) {
+        tcgetattr(STDIN_FILENO, &oldt); // old settings
+        newt = oldt; // transfer new settings
+        newt.c_lflag &= ~(ICANON | ECHO); // disable buffering & echo
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+    } else {
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // restore 
+    }  
 }
 
 char getch() {
@@ -16,7 +28,7 @@ char getch() {
     return ch;
 }
 
-void printSudoku() {
+void printSudoku(bool status) {
     char key;
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
@@ -24,12 +36,13 @@ void printSudoku() {
                 cout << "| ";
             }
             // initial sudoku from input
-            key = getch();
-            while (!strchr("123456789 ", key)) {
-                key = getch(); 
+            if (status) {
+                key = getch();
+                while (!strchr("123456789 ", key)) {
+                    key = getch(); 
+                }
+                sudoku[i][j] = key;
             }
-            sudoku[i][j] = key;
-
             // print in grid
             cout << sudoku[i][j] << ' ';
         }
@@ -39,6 +52,18 @@ void printSudoku() {
         }
     }
 }
+
+void perform() {
+    char key;
+    int row, coll;
+    printSudoku(false);
+    cout << "Enter row and collumn for completion";
+    cin >> row >> coll;
+    cout << "Enter value:";
+    cin >> key;
+    cout << "Char entered:" << key << std::flush;
+}
+
 int main() {
     // initialize sudoku matrix :3
     for (int i = 0; i < 9; i++) {
@@ -46,7 +71,11 @@ int main() {
             sudoku[i][j] = ' ';
         }
     }
-    printSudoku();
+    printSudoku(true);
+    cout << "[+] Finished reading sudoku matrix!" << endl;
+    getch();
+    cout << "\033[2J\033[1;1H"; // cls
+    perform();
     getch();
     return 0;
 
